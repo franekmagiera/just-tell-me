c3d5ddf0b8212f9911673a799fa05b3534499394
franekmagiera <framagie@gmail.com>
2024-01-14 23:22:52 +0100

    Use real summarizer

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,5 +1,4 @@
 import { JSX } from "preact";
-import { IS_BROWSER } from "$fresh/runtime.ts";
 
 export function Button(props: JSX.HTMLAttributes<HTMLButtonElement>) {
   return (
--- routes/summary/youtube.tsx
+++ routes/summary/youtube.tsx
@@ -1,6 +1,5 @@
 import { FreshContext, Handlers, PageProps } from "$fresh/server.ts";
 import { ProdAppConfig } from "../../app/src/app-config.ts";
-import { TestAppConfig } from "../../app/src/app-config.ts";
 import { InternalFailure } from "../../app/src/failure.ts";
 import { Result } from "../../app/src/result.ts";
 import { Ok } from "../../app/src/result.ts";
@@ -13,12 +12,11 @@ export const handler: Handlers = {
     ctx: FreshContext,
   ) {
     const videoId = new URL(req.url).searchParams.get("id") || "";
-    const app = await TestAppConfig.createForVideoId(videoId);
-    // const appResult = ProdAppConfig.create();
-    // if (appResult.result == Result.Failure) {
-    //   return ctx.render(appResult);
-    // }
-    // const app = appResult.data;
+    const appResult = ProdAppConfig.create();
+    if (appResult.result == Result.Failure) {
+      return ctx.render(appResult);
+    }
+    const app = appResult.data;
     const summaryResult = await app.getYoutubeVideoSummary(videoId);
     return ctx.render(summaryResult);
   },
