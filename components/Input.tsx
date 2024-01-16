12b8fce73ca473a61b51cf98b4481ba26f88caff
franekmagiera <framagie@gmail.com>
2024-01-16 18:53:54 +0100

    Change padding on the Input component

--- components/Input.tsx
+++ components/Input.tsx
@@ -4,7 +4,7 @@ export function Input(props: JSX.HTMLAttributes<HTMLInputElement>) {
   return (
     <input
       {...props}
-      className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+      className="block w-full rounded-md border-0 py-1.5 pl-3 pr-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
     />
   );
 }
